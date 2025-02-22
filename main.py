3125670f39f9de0fddd33aeff977913ca292c6af
Tae5567 <eayodej1@umbc.edu>
2025-02-22 11:46:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Task: Given a sample data folders of .txt transaction files, write software that:
+    - Reads through 2024's transaction files in all folders (get dates that include 2024)
+    Note line format in transaction file: “4,2025-01-01T16:58:53,[726107:5|553776:5],2114.235”
+    
+    Reports:
+    - Highest sales volume in a day (max sold products quantity??? and date )
+    - Highest sales value in a day( max sales amount and date)
+    - Most sold product ID by volume (max product quantity and productId, should retunr only one out od product1 and product2)
+    - Highest sales staff ID for each month. (max staffID)
+    - Highest hour of the day by average transaction volume (max hour at avg sales amount???)
+
+"""
